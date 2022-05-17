80f3b84c7f06475698a0dba9ad464f16012ae22c
L1nk0r <1kozlov1bs41@gmail.com>
2022-05-17 09:41:20 +0300

    Auth maket

--- src/components/ds_login.vue
+++ src/components/ds_login.vue
@@ -1,124 +1,155 @@
 <template>
-   <div class="wrapper_log">
-      <form>
-         <h1>Log in</h1>
+  <div class="wrapper_log">
+    <form>
+      <h1>Log in</h1>
 
-         <h3>Логин</h3>
-         <input placeholder="login..."/>
+      <h3>Логин</h3>
+      <input placeholder="login..." />
 
-         <h3>Пароль</h3>
-         <input placeholder="password..." type="password" />
+      <h3>Пароль</h3>
+      <input placeholder="password..." type="password" />
 
-         <p>Если всё ещё нет аккаунта, <router-link to="/reg"><a href="#">зарегистрируйтесь</a></router-link> прямо сейчас!</p>
+      <p>
+        Если всё ещё нет аккаунта,
+        <router-link to="/reg"><a href="#">зарегистрируйтесь</a></router-link>
+        прямо сейчас!
+      </p>
 
-         <button @click="submit()">Войти</button>
-
-      </form>
-   </div>
+      <button @click="submit()">Войти</button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default{
-   data(){
-      return {}
-   },
-   methods:{
-      submit(){
-         this.$router.push('/tests');
-      }
-   }
- }
+import User from "../models/user";
+
+export default {
+  data() {
+    return {
+      user: new User("", ""),
+    };
+  },
+  methods: {
+    submit() {
+      this.$router.push("/tests");
+    },
+    handleLogin() {
+      this.loading = true;
+      this.$store.dispatch("auth/login", this.user).then(
+        () => {
+          this.$router.push("/profile");
+        },
+        (error) => {
+          this.loading = false;
+          this.message =
+            (error.response && error.response.data) ||
+            error.message || error.toString();
+        }
+      );
+    },
+  },
+  computed: {
+    loggedIn() {
+      return this.$store.state.auth.status.loggedIn;
+    },
+  },
+  created() {
+    if (this.loggedIn) {
+      this.$router.push("/tests");
+    }
+  },
+};
 </script>
 
 <style scoped>
-   @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap");
 
-   .wrapper_log{
-      height: 100vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background: url("@/assets/background.png");
-      background-repeat: no-repeat;
-   }
+.wrapper_log {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url("@/assets/background.png");
+  background-repeat: no-repeat;
+}
 
-   form{
-      padding: 30px;
-      border: 1px solid black;
-      border-radius: 15px;
-      flex-wrap: wrap;
-   }
+form {
+  padding: 30px;
+  border: 1px solid black;
+  border-radius: 15px;
+  flex-wrap: wrap;
+}
 
-   form input{
-      font-family: 'Montserrat';
+form input {
+  font-family: "Montserrat";
 
-      border: none;
-      font-size: 20px;
-      width: 100%;
+  border: none;
+  font-size: 20px;
+  width: 100%;
 
-      color: rgba(191, 159, 128, 0.3);
+  color: rgba(191, 159, 128, 0.3);
 
-      border-bottom: 1px solid rgba(191, 159, 128, 0.3);
+  border-bottom: 1px solid rgba(191, 159, 128, 0.3);
 
-      transition: 0.4s;
-   }
+  transition: 0.4s;
+}
 
-   form h3{
-      color: rgba(191, 159, 128, 0.9);
-      font-family: 'Montserrat';
-   }
+form h3 {
+  color: rgba(191, 159, 128, 0.9);
+  font-family: "Montserrat";
+}
 
-   form a{
-      text-decoration: none;
-      color: #232EC1;
-   }
+form a {
+  text-decoration: none;
+  color: #232ec1;
+}
 
-   ::placeholder{
-      color: rgba(191, 159, 128, 0.3);
-   }
+::placeholder {
+  color: rgba(191, 159, 128, 0.3);
+}
 
-   form input:focus{
-      outline: none;
-      color: rgba(191, 159, 128, 0.9);
-      border-bottom: 1px solid rgba(191, 159, 128, 0.9);
-   }
+form input:focus {
+  outline: none;
+  color: rgba(191, 159, 128, 0.9);
+  border-bottom: 1px solid rgba(191, 159, 128, 0.9);
+}
 
-   form h1{
-      font-family: Snyder Speed Brush Cyr;
-   }
+form h1 {
+  font-family: Snyder Speed Brush Cyr;
+}
 
-   form p{
-      margin-top: 20px;
-      font-family: 'Montserrat';
-   }
+form p {
+  margin-top: 20px;
+  font-family: "Montserrat";
+}
 
-   form button{
-      margin-top: 20px;
-      margin-left: 25%;
-      border: none;
+form button {
+  margin-top: 20px;
+  margin-left: 25%;
+  border: none;
 
-      background: rgba(191, 159, 128, 0.7);
+  background: rgba(191, 159, 128, 0.7);
 
-      font-family: 'Montserrat';
+  font-family: "Montserrat";
 
-      box-sizing: border-box;
-      border-radius: 15px;
+  box-sizing: border-box;
+  border-radius: 15px;
 
-      transition: 0.6s;
-      padding: 15px;
-      width: 50%;
-      font-size: 24px;
-      color: #fff;
-   }
+  transition: 0.6s;
+  padding: 15px;
+  width: 50%;
+  font-size: 24px;
+  color: #fff;
+}
 
-   form button:hover{
-      cursor: pointer;
+form button:hover {
+  cursor: pointer;
 
-      background: rgba(191, 159, 128, 1);
-   }
+  background: rgba(191, 159, 128, 1);
+}
 
-   @font-face {
-      font-family: Snyder Speed Brush Cyr;
-      src: url("../assets/snyderspeedbrushcyr_regular.ttf");
-   }
+@font-face {
+  font-family: Snyder Speed Brush Cyr;
+  src: url("../assets/snyderspeedbrushcyr_regular.ttf");
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,21 +5,20 @@ import App from "./App.vue";
 
 import axios from "axios";
 
-import { AuthApi } from "./auth/AuthApi";
-
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 
 import Test from "./elements/test";
 import TestAnalyze from "./elements/analyze";
 
+import { auth } from "./auth.modules"
+
 const store = createStore({
+  modules:{
+    auth
+  },
   state: {
     BACKEND_URL: "http://localhost:3000",
-    credentials: {
-      token: localStorage.getItem("token") || null,
-      userRole: localStorage.getItem("userRole") || null,
-    },
     tests: [],
     new_test: {
       name: "No name",
@@ -36,22 +35,6 @@ const store = createStore({
     result_name: null,
   },
   mutations: {
-    setToken(state, token) {
-      state.credentials.token = token;
-      localStorage.setItem("token", token);
-    },
-    setUserRole(state, userRole) {
-      state.credentials.userRole = userRole;
-      localStorage.setItem("userRole", userRole);
-    },
-    deleteToken(state) {
-      state.credentials.token = null;
-      localStorage.removeItem("token");
-    },
-    deleteUserRole(state) {
-      state.credentials.userRole = null;
-      localStorage.removeItem("userRole");
-    },
     SET_TESTS_TO_STATE: (state, tests) => {
       tests.forEach((element) => {
         let test = new Test(element);
@@ -109,16 +92,6 @@ const store = createStore({
     },
   },
   actions: {
-    onLogin({ commit }, { login, password }) {
-      AuthApi.login({ login, password }).then((res) => {
-        commit("setToken", res.token);
-        commit("setUserRole", res.userRole);
-      });
-    },
-    onLogout({ commit }) {
-      commit("deleteToken");
-      commit("deleteUserRole");
-    },
     GET_TESTS_FROM_API({ commit }) {
       return axios(`${this.getters.getServerUrl}/tests`, {
         method: "GET",
@@ -228,7 +201,6 @@ const store = createStore({
     getServerUrl: (state) => {
       return state.BACKEND_URL;
     },
-    getUserRole: (state) => state.credentials.userRole,
     TESTS(state) {
       return state.tests;
     },
